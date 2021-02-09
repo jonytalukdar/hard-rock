5739aa922f033c571f10281c3ceba92fe3a65e77
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-09 14:12:54 +0600

    added apis link

---
+++ api.js
@@ -0,0 +1,4 @@
+// api link :  https://api.lyrics.ovh/suggest/:searchText
+// example : https://api.lyrics.ovh/suggest/hello
+// lyrics link :  https://api.lyrics.ovh/v1/:artist/:title
+// example : https://api.lyrics.ovh/v1/Adele/Hello
