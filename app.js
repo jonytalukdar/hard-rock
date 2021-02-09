e33394030885c4132799d88449b947f3bea771db
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-09 21:21:38 +0600

    completed fetch section

---
+++ app.js
@@ -0,0 +1,7 @@
+const searchSongs = () => {
+  const searchText = document.getElementById('search-field').value;
+  const url = `https://api.lyrics.ovh/suggest/:${searchText}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
